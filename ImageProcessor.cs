2c21cb56063d696a4f13563deb0be2680c73db18
jtemte3 <47343054+jtemte3@users.noreply.github.com>
2024-01-09 18:39:32 -0600

    added color replacement for layer images

--- ImageProcessor.cs
+++ ImageProcessor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Drawing;
 using Image = System.Drawing.Image;
 using System.Drawing.Imaging;
@@ -23,12 +24,14 @@ namespace CrestMaker
             Image imageBackground = Image.FromFile("BaseImages/Background.png");
             Image imageOverlay = Image.FromFile("BaseImages/Overlay-2.png");
 
+            Image coloredBackground = ChangeNonAlphaColor(imageBackground, Color.Blue);
+
             Image img = new Bitmap(1024, 1024);
             using (Graphics gr = Graphics.FromImage(img))
             {
 
                 //gr.DrawImage(imageBackground, new Point(0, 0));
-                gr.DrawImageUnscaled(imageBackground, 0, 0, img.Width, img.Height);
+                gr.DrawImageUnscaled(coloredBackground, 0, 0, img.Width, img.Height);
                 gr.DrawImageUnscaled(imageOverlay, 0, 0, img.Width, img.Height);
             }
 
@@ -51,6 +54,30 @@ namespace CrestMaker
 
             return CrestImage;
         }
+
+        /// <summary>
+        /// This funtion changes the white pixels of any image to a custom color
+        /// </summary>
+        private Image ChangeNonAlphaColor(Image img, Color color)
+        {
+            Bitmap bmp = new Bitmap(img);
+            for (int x = 0; x < bmp.Width; x++)
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    Color currentColor = bmp.GetPixel(x, y);
+                    //Check if the pixel is not clear, and if all of the RGB values are 255 (white)
+                    //This will keep any black outlines in the layer
+                    if (currentColor.A != 0 && currentColor.R == 255 && currentColor.G == 255 && currentColor.B == 255)
+                    {
+                        bmp.SetPixel(x, y, color);
+                    }
+                }
+            }
+            return (Image)bmp;
+
+        }
         #endregion
     }
+    
 }
